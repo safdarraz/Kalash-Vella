d197f0c1bcb10df3456691b85ea7ab4c86d44033
LegendSaqib <muhammadsaqib6902@gmail.com>
2025-11-14 14:50:29 +0500

    project uploaded

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -9,8 +9,9 @@ router.post("/", async (req, res) => {
   const { firstName, lastName, email, phone, message } = req.body;
 
   const transporter = nodemailer.createTransport({
-    host: "gmail",
-    port: 2525,
+    // host: "gmail",
+    host: "smtp.gmail.com",
+    port: 465,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
